82e131e3c57408a327e6a759c96c8d02118dfba7
EmmTarSystem <manu974.training@gmail.com>
2025-06-30 19:55:46 +0200

    android studio build ok

--- android/app/src/main/java/fr/taralab/monsuivisportif/MainActivity.java
+++ android/app/src/main/java/fr/taralab/monsuivisportif/MainActivity.java
@@ -1,5 +1,40 @@
 package fr.taralab.monsuivisportif;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    private SoftInputAssist softInputAssist;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        softInputAssist = new SoftInputAssist(this);
+        softInputAssist.onResume();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (softInputAssist != null) {
+            softInputAssist.onResume();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        if (softInputAssist != null) {
+            softInputAssist.onPause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        if (softInputAssist != null) {
+            softInputAssist.onDestroy();
+        }
+        super.onDestroy();
+    }
+}
