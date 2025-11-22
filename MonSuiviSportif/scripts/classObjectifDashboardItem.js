f24f0b9aa29ae0acffd6c2a9e277bedba8c0ee84
EmmTarSystem <manu974.training@gmail.com>
2025-11-22 18:01:25 +0100

    Objectif : creation de la class pour l'item dashboard

---
+++ MonSuiviSportif/scripts/classObjectifDashboardItem.js
@@ -0,0 +1,47 @@
+class ObjectifDashboardItem {
+    constructor(activityName,textSuiviTarget,textCurrentValue,imgRef,progressValue,categoryColor,parentRef){
+        this.activityName = activityName;
+        this.textSuiviTarget = textSuiviTarget;
+        this.textCurrentValue = textCurrentValue;
+        this.imgRef = imgRef;
+        this.progressValue = progressValue;
+        this.categoryColor = categoryColor;
+        this.parentRef = parentRef;
+
+
+        // Conteneur principal
+        this.element = document.createElement("div");
+        this.element.classList.add("objectif-card");
+
+        // Fonction de rendu
+        this.render();
+
+        // Insertion dans le parent
+        this.parentRef.appendChild(this.element);
+
+    }
+
+
+
+    render(){
+        this.element.innerHTML = `
+            <div class="objectif-top-row">
+                <div class="objectif-card-icon">
+                    <img src=${this.imgRef}>
+                </div>
+                <div class="objectif-progress-badge" style="--progress: ${this.progressValue} ; --bar-color:${this.categoryColor}">
+                    <svg viewBox="0 0 36 36">
+                        <path class="objectif-track" d="M18 2.5
+                            a 15.5 15.5 0 1 1 0 31
+                            a 15.5 15.5 0 1 1 0 -31" />
+                        <path class="objectif-progress" d="M18 2.5
+                            a 15.5 15.5 0 1 1 0 31
+                            a 15.5 15.5 0 1 1 0 -31" />
+                    </svg>
+                    <div class="objectif-card-value">${this.textCurrentValue}</div>
+                </div>
+            </div>
+            <p>${this.textSuiviTarget}</p>
+        `;
+    }
+}
